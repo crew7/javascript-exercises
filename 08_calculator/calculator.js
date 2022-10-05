bc984ef1220017f5d737cffaf8401af143d8e479
Tyler <tyjesp@gmail.com>
2022-10-05 19:50:35 -0400

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+  return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(arrayData) {
+	return arrayData.reduce( function(accumulator, currentValue) {
+    return accumulator += currentValue
+  }, 0 )
 };
 
-const multiply = function() {
-
+const multiply = function(arrayData) {
+  return arrayData.reduce( function(accumulator, currentValue) {
+    return accumulator *= currentValue
+  }, )
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+	return number1 ** number2;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialNumber) {
+  if (factorialNumber === 0) return 1;
+	for (let i = factorialNumber - 1; i > 1; --i) {
+    factorialNumber *= i;
+  }
+  return factorialNumber;
 };
 
 // Do not edit below this line
